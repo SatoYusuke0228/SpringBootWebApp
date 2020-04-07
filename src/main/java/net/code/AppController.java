0ae56ece0565b7fa7d6d6756250e8f081242d6d8
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-04-08 04:45:56 +0900

    コミット前にファイル保存の失念のため追加

--- src/main/java/net/code/AppController.java
+++ src/main/java/net/code/AppController.java
@@ -28,10 +28,10 @@ public class AppController {
 //		全商品取得
 		List<TrProductEntity> productList = productService.findAll();
 
-		for (int i = 0; i < productList.size(); i++) {
-			System.out.println("取得したファイル名");
-			//System.out.println(productList.get(i).getProductPhotoFileName1());
-		}
+//		for (int i = 0; i < productList.size(); i++) {
+//			System.out.println("取得したファイル名");
+//			System.out.println(productList.get(i).getProductPhotoFileName1());
+//		}
 
 //		取得した全販売商品データをsessionスコープに保存
 		session.setAttribute("productList", productList);
