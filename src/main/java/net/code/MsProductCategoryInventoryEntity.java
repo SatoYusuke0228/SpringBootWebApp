c84fd3c86a78aed3ae5b3bc6cc6d26162029fb2c
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-04-27 18:05:18 +0900

    lombokライブラリを使用可能にする依存関係を追加

--- src/main/java/net/code/MsProductCategoryInventoryEntity.java
+++ src/main/java/net/code/MsProductCategoryInventoryEntity.java
@@ -10,6 +10,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 商品カテゴリーテーブルのフィールドの宣言及びカプセル化
  *
@@ -40,97 +43,46 @@ public class MsProductCategoryInventoryEntity {
 
 	@Id
 	@Column(name = "PRODUCT_CATEGORY_ID", nullable = false, length = 1)
+	@Getter
+	@Setter
 	private String productCategoryId;
 
 	@Column(name = "PRODUCT_CATEGORY_NAME", nullable = false, length = 64)
+	@Getter
+	@Setter
 	private String productCategoryName;
 
 	@Column(name = "INSERT_DATE", nullable = false)
+	@Getter
+	@Setter
 	private Timestamp insertDate;
 
 	@Column(name = "INSERT_USER", nullable = false, length = 64)
+	@Getter
+	@Setter
 	private String insertUser;
 
 	@Column(name = "UPDATE_DATE", nullable = false)
+	@Getter
+	@Setter
 	private Timestamp updateDate;
 
 	@Column(name = "UPDATE_USER", nullable = false, length = 64)
+	@Getter
+	@Setter
 	private String updateUser;
 
 	@Column(name = "DELETE_DATE", nullable = true)
+	@Getter
+	@Setter
 	private Timestamp deleteDate;
 
 	@Column(name = "DELETE_USER", nullable = true, length = 64)
+	@Getter
+	@Setter
 	private String deleteUser;
 
 	@OneToMany //(mappedBy = "fk")
 	@JoinColumn(name="PRODUCT_CATEGORY_ID")
 	private List<TrProductEntity> trProductEntity;
-
-	/**
-	 * getter and setter
-	 */
-	public String getProductCategoryId() {
-		return productCategoryId;
-	}
-
-	public void setProductCategoryId(String productCategoryId) {
-		this.productCategoryId = productCategoryId;
-	}
-
-	public String getProductCategoryName() {
-		return productCategoryName;
-	}
-
-	public void setProductCategoryName(String productCategoryName) {
-		this.productCategoryName = productCategoryName;
-	}
-
-	public Timestamp getInsertDate() {
-		return insertDate;
-	}
-
-	public void setInsertDate(Timestamp insertDate) {
-		this.insertDate = insertDate;
-	}
-
-	public String getInsertUser() {
-		return insertUser;
-	}
-
-	public void setInsertUser(String insertUser) {
-		this.insertUser = insertUser;
-	}
-
-	public Timestamp getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(Timestamp updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public String getUpdateUser() {
-		return updateUser;
-	}
-
-	public void setUpdateUser(String updateUser) {
-		this.updateUser = updateUser;
-	}
-
-	public Timestamp getDeleteDate() {
-		return deleteDate;
-	}
-
-	public void setDeleteDate(Timestamp deleteDate) {
-		this.deleteDate = deleteDate;
-	}
-
-	public String getDeleteUser() {
-		return deleteUser;
-	}
-
-	public void setDeleteUser(String deleteUser) {
-		this.deleteUser = deleteUser;
-	}
 }
--- src/main/java/net/code/TrProductEntity.java
+++ src/main/java/net/code/TrProductEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 商品テーブルのフィールドの宣言及びカプセル化
  *
@@ -39,188 +42,86 @@ public class TrProductEntity {
 
 	@Id
 	@Column(name = "PRODUCT_ID", nullable = false, length = 16)
+	@Getter
+	@Setter
 	private String productId;
 
 	@Column(name = "PRODUCT_NAME", nullable = false, length = 128)
+	@Getter
+	@Setter
 	private String productName;
 
 	@Column(name = "PRODUCT_SELLING_PRICE", nullable = false)
+	@Getter
+	@Setter
 	private int productPrice;
 
 	@Column(name = "PRODUCT_CATEGORY_ID", nullable = false, length = 1)
+	@Getter
+	@Setter
 	private String productCategoryId;
 
 	@Column(name = "PRODUCT_STOCK", nullable = false)
+	@Getter
+	@Setter
 	private int productStock;
 
 	@Column(name = "PRODUCT_COMMENT", nullable = true, length = 2048)
+	@Getter
+	@Setter
 	private String productComment;
 
 	@Column(name = "PRODUCT_PHOTO_FILE_NAME1", nullable = true, length = 256)
+	@Getter
+	@Setter
 	private String productPhotoFileName1;
 
 	@Column(name = "PRODUCT_PHOTO_FILE_NAME2", nullable = true, length = 256)
+	@Getter
+	@Setter
 	private String productPhotoFileName2;
 
 	@Column(name = "PRODUCT_PHOTO_FILE_NAME3", nullable = true, length = 256)
+	@Getter
+	@Setter
 	private String productPhotoFileName3;
 
 	@Column(name = "PRODUCT_SHOW_FLAG", nullable = false)
+	@Getter
+	@Setter
 	private String productShowFlag;
 
 	@Column(name = "INSERT_DATE", nullable = false)
+	@Getter
+	@Setter
 	private Timestamp insertDate;
 
 	@Column(name = "INSERT_USER", nullable = false, length = 64)
+	@Getter
+	@Setter
 	private String insertUser;
 
 	@Column(name = "UPDATE_DATE", nullable = false)
+	@Getter
+	@Setter
 	private Timestamp updateDate;
 
 	@Column(name = "UPDATE_USER", nullable = false, length = 64)
+	@Getter
+	@Setter
 	private String updateUser;
 
 	@Column(name = "DELETE_DATE", nullable = true)
+	@Getter
+	@Setter
 	private Timestamp deleteDate;
 
 	@Column(name = "DELETE_USER", nullable = true, length = 64)
+	@Getter
+	@Setter
 	private String deleteUser;
 
 	//	@ManyToOne(fetch = FetchType.LAZY)
 	//	@JoinColumn(name = "productCategoryFK", referencedColumnName = "PRODUCT_CATEGORY_ID")
 	//	private MsProductCategoryInventoryEntity fk;
-
-	/**
-	 * setter and getter
-	 * @parm setter
-	 * @return getter
-	 */
-
-	public String getProductId() {
-		return productId;
-	}
-
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public int getProductPrice() {
-		return productPrice;
-	}
-
-	public void setProductPrice(int productPrice) {
-		this.productPrice = productPrice;
-	}
-
-	public String getProductCategoryId() {
-		return productCategoryId;
-	}
-
-	public void setProductCategoryId(String productCategoryId) {
-		this.productCategoryId = productCategoryId;
-	}
-
-	public int getProductStock() {
-		return productStock;
-	}
-
-	public void setProductStock(int productStock) {
-		this.productStock = productStock;
-	}
-
-	public String getProductComment() {
-		return productComment;
-	}
-
-	public void setProductComment(String productComment) {
-		this.productComment = productComment;
-	}
-
-	public String getProductPhotoFileName1() {
-		return productPhotoFileName1;
-	}
-
-	public void setProductPhotoFileName1(String productPhotoFileName1) {
-		this.productPhotoFileName1 = productPhotoFileName1;
-	}
-
-	public String getProductPhotoFileName2() {
-		return productPhotoFileName2;
-	}
-
-	public void setProductPhotoFileName2(String productPhotoFileName2) {
-		this.productPhotoFileName2 = productPhotoFileName2;
-	}
-
-	public String getProductPhotoFileName3() {
-		return productPhotoFileName3;
-	}
-
-	public void setProductPhotoFileName3(String productPhotoFileName3) {
-		this.productPhotoFileName3 = productPhotoFileName3;
-	}
-
-	public String getProductShowFlag() {
-		return productShowFlag;
-	}
-
-	public void setProductShowFlag(String productShowFlag) {
-		this.productShowFlag = productShowFlag;
-	}
-
-	public Timestamp getInsertDate() {
-		return insertDate;
-	}
-
-	public void setInsertDate(Timestamp insertDate) {
-		this.insertDate = insertDate;
-	}
-
-	public String getInsertUser() {
-		return insertUser;
-	}
-
-	public void setInsertUser(String insertUser) {
-		this.insertUser = insertUser;
-	}
-
-	public Timestamp getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(Timestamp updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public String getUpdateUser() {
-		return updateUser;
-	}
-
-	public void setUpdateUser(String updateUser) {
-		this.updateUser = updateUser;
-	}
-
-	public Timestamp getDeleteDate() {
-		return deleteDate;
-	}
-
-	public void setDeleteDate(Timestamp deleteDate) {
-		this.deleteDate = deleteDate;
-	}
-
-	public String getDeleteUser() {
-		return deleteUser;
-	}
-
-	public void setDeleteUser(String deleteUser) {
-		this.deleteUser = deleteUser;
-	}
 }
