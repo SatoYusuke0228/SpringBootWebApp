c1a5b96d6c146c714bd69db5a49ece76450f2075
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-05-08 17:43:43 +0900

    不要なバージョン指定表記を削除

---
+++ src/main/java/net/code/TrProductEntity_.java
@@ -0,0 +1,24 @@
+package net.code;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ * 商品テーブルのメタモデルクラス
+ *
+ * アプリケーションの商品検索に使用する。
+ * メタモデルクラスからカラム名を取得するやり方の方が
+ * より変更に強く堅牢な作りになる（らしい）
+ *
+ * @author 佐藤由佑
+ */
+
+@StaticMetamodel(TrProductEntity.class)
+public class TrProductEntity_ {
+
+	public static volatile SingularAttribute<TrProductEntity, String> productId;
+	public static volatile SingularAttribute<TrProductEntity, String> productName;
+
+	public static final String PRODUCT_ID = "productId";
+	public static final String PRODUCT_Name = "productName";
+}
